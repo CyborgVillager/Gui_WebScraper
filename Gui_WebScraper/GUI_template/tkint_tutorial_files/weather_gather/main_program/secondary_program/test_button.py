9760b55f27368110beb15caade49c0b1cfeb4b07
CyborgVillager <almawijonathan@gmail.com>
2020-01-20 14:25:51 -0600

    v15 API Aquired
    API Aquired : aquired API from https://openweathermap.org/forecast5. User can now scrap basic info of the state
                        /city they are choosing from
                        template to use -> api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
    
                        Updated test_button.py to include the API functionality & updated weather_app.py @ line 36 to
                        have get_weather instead of test_function

--- Gui_WebScraper/GUI_template/tkint_tutorial_files/weather_gather/main_program/secondary_program/test_button.py
+++ Gui_WebScraper/GUI_template/tkint_tutorial_files/weather_gather/main_program/secondary_program/test_button.py
@@ -1,3 +1,12 @@
+import requests
 
 def test_function(entry):
     print('Button Clicked',entry)
+
+
+def get_weather(city):
+    weather_key = '3d7102c18ab158257bce2ab1be33d4df'
+    url = 'https://api.openweathermap.org/data/2.5/forecast'
+    params = {'APPID':weather_key,'q':city,'units':'imperial'}
+    response = requests.get(url,params=params)
+    print(response.json())
