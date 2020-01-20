ad04da329d52ff2f3d9dd3b84f25286970f8e04b
CyborgVillager <almawijonathan@gmail.com>
2020-01-20 12:36:58 -0600

    v9 Align Info
    Align Info : align button,text/input as reference

--- Gui_WebScraper/GUI_template/tkint_tutorial_files/weather_gather/weather_app.py
+++ Gui_WebScraper/GUI_template/tkint_tutorial_files/weather_gather/weather_app.py
@@ -19,15 +19,17 @@ frame = Frame(root,bg='#99b3ff')
 
 frame.place(relx=0.1, rely=0.1,relwidth=0.8,relheight=0.8)
 button = Button(frame, text='Testing Button', bg='Gray')
-button.pack(side="left")
+# button.pack(side="left", fill='both') -> example on filing the whole left screen to an extent
+# button.pack(side="left", fill='both', expand=True) -> bigger
+button.grid(row=0,column=0)
 
 # Just a Label
 label = Label(frame, text='Hello this is a label', bg='yellow')
-label.pack(side='right')
+label.grid(row=0,column=1)
 
 # User Typing Enabled
 entry = Entry(frame, bg='green')
-entry.pack(side='bottom')
+entry.grid(row=0,column=2)
 
-button.pack()
+
 root.mainloop()
