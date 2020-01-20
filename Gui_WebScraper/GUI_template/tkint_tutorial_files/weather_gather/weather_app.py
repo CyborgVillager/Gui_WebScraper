b57fd3518ff495e1c0ccb4e36db1c35e31ae6ad0
CyborgVillager <almawijonathan@gmail.com>
2020-01-20 10:55:14 -0600

    v7 Button,Label & User Input
    Button,Label & User Input: Template created/ reference to be used for later use.

--- Gui_WebScraper/GUI_template/tkint_tutorial_files/weather_gather/weather_app.py
+++ Gui_WebScraper/GUI_template/tkint_tutorial_files/weather_gather/weather_app.py
@@ -19,7 +19,15 @@ frame = Frame(root,bg='#99b3ff')
 
 frame.place(relx=0.1, rely=0.1,relwidth=0.8,relheight=0.8)
 button = Button(frame, text='Testing Button', bg='Gray')
+button.pack()
 
+# Just a Label
+label = Label(frame, text='Hello this is a label', bg='yellow')
+label.pack()
+
+# User Typing Enabled
+entry = Entry(frame, bg='green')
+entry.pack()
 
 button.pack()
 root.mainloop()
