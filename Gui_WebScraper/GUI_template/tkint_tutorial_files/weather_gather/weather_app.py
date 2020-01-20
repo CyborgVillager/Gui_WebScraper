f1b42f36f7b650bf8e6f5a21fcacef52e2d89c38
CyborgVillager <almawijonathan@gmail.com>
2020-01-20 13:03:05 -0600

    v11 Lower_Frame , Border: Lower_Frame & Border created to provide layout for the program -> weather_app.py
    Lower_Frame , Border: Lower_Frame & Border created to provide layout for the program -> weather_app.py

--- Gui_WebScraper/GUI_template/tkint_tutorial_files/weather_gather/weather_app.py
+++ Gui_WebScraper/GUI_template/tkint_tutorial_files/weather_gather/weather_app.py
@@ -11,26 +11,30 @@ root = Tk()
 canvas = Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-# To organize widgets use Frame
-frame = Frame(root,bg='#99b3ff')
+# To organize widgets use Frame , bd = border
+frame = Frame(root,bg='#99b3ff', bd=5)
 # relwidth = relative width, relheight= relative height
 # This makes the frame centered using relx=0.1, rely=0.1 -> Also adjusts with the user's screen
 # frame.place(relx=0.1, rely=0.1,relwidth=0.8,relheight=0.8)
-
-frame.place(relx=0.1, rely=0.1,relwidth=0.8,relheight=0.8)
-button = Button(frame, text='Testing Button', bg='Gray')
+frame.place(relx=0.5, rely=0.1,relwidth=0.75,relheight=0.1,anchor='n')
+button = Button(frame, text='Testing Button', font=40)
 # button.pack(side="left", fill='both') -> example on filing the whole left screen to an extent
 # button.pack(side="left", fill='both', expand=True) -> bigger
 # button.grid(row=0,column=0)
-button.place(relx=0,rely=0,relwidth=0.25,relheight=0.25)
+button.place(relx=.7,relheight=1,relwidth=0.3)
 
-# Just a Label
-label = Label(frame, text='Hello this is a label', bg='yellow')
-label.place(relx=0.3,rely=0, relwidth=0.45,relheight=.25)
 
 # User Typing Enabled
-entry = Entry(frame, bg='green')
-entry.place(relx=0.8,rely=0,relwidth=.2,relheight=.3)
+entry = Entry(frame, font=40)
+entry.place(relwidth=.65,relheight=1)
+
+
+lower_frame = Frame(root,bg='#99b3ff', bd=10)
+lower_frame.place(relx=.5, rely=.25, relwidth=.75,relheight=.6,anchor='n')
+
+# Just a Label
+label = Label(lower_frame, text='Hello this is a label', bg='yellow')
+label.place(relx=0.3,rely=0, relwidth=0.45,relheight=.25)
 
 
 root.mainloop()
